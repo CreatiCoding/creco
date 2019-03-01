27e3f5e7e6487e8b6c0e36e8dec048f05ece3549
CreatiCoding <creaticoding@gmail.com>
2019-03-02 01:50:25 +0900

    feat(tsjest): success for ts-koa-router jest test

--- portfolio/portfolio-backend/test/example.test.ts
+++ portfolio/portfolio-backend/test/example.test.ts
@@ -1,40 +1,8 @@
-// import * as request from "supertest";
-import * as app from "../app";
-import * as http from "http";
-import * as request from "request";
+import * as request from 'supertest';
+import * as app from '../app';
 
-let server: http.Server;
-const isRunning = true;
-
-beforeAll(() => {
-  if (!isRunning) {
-    var port = "3000";
-    server = http.createServer(app);
-    server.listen(port);
-  }
-});
-afterAll(() => {
-  if (!isRunning) {
-    server.close();
-  }
-});
-
-describe("basic tests", () => {
-  test("ts-jest test", () => {
-    const expected: number = 1;
-    const received: number = 1;
-    expect(expected).toEqual(received);
-  });
-  test("ts-jest server", async () => {
-    const expected: number = 1;
-    const received: number = 1;
-    request.get({ url: "http://127.0.0.1:3000/str", json: true }, function(
-      e,
-      r,
-      user
-    ) {
-      console.log(r);
-    });
-    expect(expected).toEqual(received);
-  });
+test('Hello world works', async () => {
+    const response = await request(app.callback()).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('true');
 });
